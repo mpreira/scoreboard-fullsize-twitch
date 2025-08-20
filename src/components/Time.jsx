d593890cb5f6d1ac7be6d7c38d2dfeb8b2367789
mpreira <mlpreira@gmail.com>
2025-08-20 13:57:14 +0200

    css timer

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -1,6 +1,8 @@
 import '../utils/Time';
 
-const Time = () =>{
+const Time = () => {
+    // This component displays the current time in a formatted manner
+
     return(
         <div className="time" id="time">
             <span id="minutes">00</span>:<span id="seconds">00</span>
