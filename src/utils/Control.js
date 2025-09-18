edcce8527a8298b427b74754dcdeccb1fe70c5fd
Marie-Louise Preira <65363552+mpreira@users.noreply.github.com>
2025-09-18 16:43:35 +0200

    fix urgent couleur nz

--- src/utils/Control.js
+++ src/utils/Control.js
@@ -82,7 +82,7 @@ const teamColors = {
     Brasil: { primary: "--brasil-green", secondary: "--brasil-yellow", text: "--brasil-blue" },
     Italy: { primary: "--italy-blue", secondary: "--england-white", text: "--italy-red" },
     RSA: { primary: "--rsa-gold", secondary: "--rsa-green", text: "--rsa-gold" },
-    NZL: { primary: "--nzl-black", secondary: "--nzl-silver", text: "--nzl-silver" },
+    NZL: { primary: "--nzl-black", secondary: "--nzl-silver", text: "--nzl-black" },
     Ireland: { primary: "--ireland-green", secondary: "--england-white", text: "--ireland-green" },
     Australia: { primary: "--australia-gold", secondary: "--england-white", text: "--australia-gold" },
     Canada: { primary: "--canada-red", secondary: "--england-white", text: "--canada-red" },
